0faa38da758f72df01f1ab4ed8b932cacb9e6d7e
DreF <fiadotsyeu@gmail.com>
2024-05-05 14:35:39 +0200

    Added file Extensions. Functions for handling tab position have been added, and a mask for the tab bar.

---
+++ backlog/Service/Extensions.swift
@@ -0,0 +1,59 @@
+//
+//  Extensions.swift
+//  backlog
+//
+//  Created by Andrei Fiadotsyeu on 05.05.2024.
+//
+
+import SwiftUI
+
+struct OffsetKey: PreferenceKey {
+    static var defaultValue: CGFloat = .zero
+    static func reduce( value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func offsetX(completion: @escaping (CGFloat) -> ()) -> some View {
+        self
+            .overlay {
+                GeometryReader {
+                    let minX = $0.frame(in: .scrollView(axis: .horizontal)).minX
+                    
+                    Color.clear
+                        .preference(key: OffsetKey.self, value: minX)
+                        .onPreferenceChange(OffsetKey.self, perform: completion)
+                }
+            }
+    }
+    
+    // tab bar masking
+    @ViewBuilder
+    func tabMask(_ tabProgress: CGFloat) -> some View {
+        ZStack {
+            self
+                .foregroundStyle(.gray)
+            
+            self
+                .symbolVariant(.fill)
+                .mask {
+                    GeometryReader {
+                        let size = $0.size
+                        let capsuleWidth = size.width / CGFloat(Tab.allCases.count)
+                        
+                        Capsule()
+                            .frame(width: capsuleWidth)
+                            .offset(x: tabProgress * (size.width - capsuleWidth))
+                    }
+                }
+        }
+    }
+}
+
+
+#Preview {
+    Home()
+}
+
