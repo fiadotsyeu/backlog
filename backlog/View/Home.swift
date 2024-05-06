66b81e4756aa5917a4a3198c2330ec514cc2ba1a
DreF <fiadotsyeu@gmail.com>
2024-05-06 08:31:32 +0200

    Added GeometryReader. Now the menu switches correctly.

--- backlog/View/Home.swift
+++ backlog/View/Home.swift
@@ -6,10 +6,18 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct Home: View {
     @State private var selectedTab: Tab?
     @Environment(\.colorScheme) private var scheme
+    @State private var tabProgress: CGFloat = 0
+    @State private var searchText = ""
+    @Query private var items: [Item]
+
+    private var searchResults : [Item] {
+        searchText.isEmpty ? items : items.filter { $0.title.contains(searchText) }
+    }
     
     var body: some View {
         VStack(spacing: 15) {
@@ -21,7 +29,7 @@ struct Home: View {
                 Spacer()
                 
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Image(systemName: "bell.badge")
+                    Image(systemName: "microbe")
                 })
             }
             .font(.title2)
@@ -33,7 +41,39 @@ struct Home: View {
             .padding(15)
             
             TabBar()
+            
+            GeometryReader {
+                let size = $0.size
+                
+                ScrollView(.horizontal) {
+                    LazyHStack(spacing: 0) {
+                        AllView()
+                            .id(Tab.all)
+                            .containerRelativeFrame(.horizontal)
+                        CategoryView()
+                            .id(Tab.categories)
+                            .containerRelativeFrame(.horizontal)
+                        SettingView()
+                            .id(Tab.settings)
+                            .containerRelativeFrame(.horizontal)
+                    }
+                    .scrollTargetLayout()
+                    .offsetX { value in
+                        // converting Offset into progress
+                        let progress = -value / (size.width * CGFloat(Tab.allCases.count - 1))
+                        tabProgress = max(min(progress, 1), 0)
+                        print(value)
+                    }
+                }
+                .scrollPosition(id: $selectedTab)
+                .scrollIndicators(.hidden)
+                .scrollTargetBehavior(.paging)
+                .scrollClipDisabled()
+            }
+            
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .background(.gray.opacity(0.1))
     }
     
     @ViewBuilder
@@ -56,9 +96,24 @@ struct Home: View {
                 }
             }
         }
+        .tabMask(tabProgress)
+        .background {
+            GeometryReader {
+                let size = $0.size
+                let capsuleWidth = size.width / CGFloat(Tab.allCases.count)
+                
+                Capsule()
+                    .fill(scheme == .dark ? .black : .white)
+                    .frame(width: capsuleWidth)
+                    .offset(x: tabProgress * (size.width - capsuleWidth))
+            }
+        }
         .background(.gray.opacity(0.1), in: .capsule)
         .padding(.horizontal, 15)
     }
+    
+    
+    
 }
 
 #Preview {
