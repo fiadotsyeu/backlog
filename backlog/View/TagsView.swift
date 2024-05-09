d2ca30e363e64722398db2dba3c407eac68ab1ee
DreF <fiadotsyeu@gmail.com>
2024-05-09 12:29:36 +0200

    Updated the TagView

--- backlog/View/TagsView.swift
+++ backlog/View/TagsView.swift
@@ -12,39 +12,83 @@ struct TagsView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
     @State private var searchText = ""
-
+    let systemImage: String
+    let titleKey: String
+    @State var isSelected: Bool
     
     var body: some View {
-        ScrollView(.vertical) {
-            LazyVGrid(columns: Array(repeating: GridItem(), count: 2), content: {
-                ForEach(1...20, id: \.self) { _ in
-                    RoundedRectangle(cornerRadius: 15)
-                        .fill(Color(.gray).gradient).opacity(0.6)
-                        .frame(height: 100)
-                        .overlay {
-                            VStack(alignment: .leading, spacing: 6) {
-                                RoundedRectangle(cornerRadius: 6)
-                                    .fill(.white.opacity(0.5))
-                                    .frame(width: 150, height: 40)
-                                RoundedRectangle(cornerRadius: 6)
-                                    .fill(.white.opacity(0.5))
-                                    .frame(width: 150, height: 17)
-                            }
-                        }
-                }
-            })
-            .padding(15)
+        
+        HStack(spacing: 4) {
+            Image.init(systemName: systemImage).font(.body)
+            Text(titleKey).font(.body).lineLimit(1)
         }
-        .scrollIndicators(.hidden)
-        .scrollClipDisabled()
-        .mask {
-            Rectangle()
-                .padding(.bottom, -100)
+        .padding(.vertical, 4)
+        .padding(.leading, 4)
+        .padding(.trailing, 10)
+        .foregroundColor(isSelected ? .white : .blue)
+        .background(isSelected ? Color.blue : Color.white)
+        .cornerRadius(20)
+        .overlay(
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(Color.blue, lineWidth: 1.5)
+                
+        ).onTapGesture {
+            isSelected.toggle()
         }
+        
+        
     } 
 }
 
+struct TagContainerView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+    @Query private var tags: [Tag]
+    
+    var body: some View {
+        var width = CGFloat.zero
+        var height = CGFloat.zero
+        return GeometryReader { geo in
+            ZStack(alignment: .topLeading, content: {
+                ForEach(tags) { tag in
+                    TagsView(systemImage: tag.systemImage ?? "bookmark.circle",
+                             titleKey: tag.titleKey,
+                             isSelected: tag.isSelected)
+                    .padding(.all, 5)
+                    .alignmentGuide(.leading) { dimension in
+                        if (abs(width - dimension.width) > geo.size.width) {
+                            width = 0
+                            height -= dimension.height
+                        }
+                        let result = width
+                        if tag.id == tags.last!.id {
+                            width = 0
+                        } else {
+                            width -= dimension.width
+                        }
+                        return result
+                    }
+                    .alignmentGuide(.top) { dimension in
+                        let result = height
+                        if tag.id == tags.last!.id {
+                            height = 0
+                        }
+                        return result
+                    }
+                }
+            })
+        }
+    }
+}
+
+
 #Preview {
-    TagsView()
+    TagsView(systemImage: "heart.circle", titleKey: "Title", isSelected: false)
         .modelContainer(for: Item.self, inMemory: true)
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
+#Preview {
+    ContentView()
+        .modelContainer(for: Item.self, inMemory: false)
 }
