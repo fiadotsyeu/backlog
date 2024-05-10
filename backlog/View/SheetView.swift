665407801e0de3695e869b3b39f9b1c360433b08
DreF <fiadotsyeu@gmail.com>
2024-05-10 12:40:03 +0200

    Added SheetView

---
+++ backlog/View/SheetView.swift
@@ -0,0 +1,18 @@
+//
+//  SheetView.swift
+//  backlog
+//
+//  Created by Andrei Fiadotsyeu on 10.05.2024.
+//
+
+import SwiftUI
+
+struct SheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetView()
+}
