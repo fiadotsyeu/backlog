583007d80cfb1557bf8cb76cea6cdbbf0b28dab1
DreF <fiadotsyeu@gmail.com>
2024-05-19 12:18:14 +0200

    ColorModel class: Stores color components (red, green, blue, alpha) and provides methods for creating UIColor and Color. Added a static method 'from(color: Color)' for creating ColorModel from Color. Added 'swiftUIColor' for converting ColorModel to Color.

--- backlog/Model/Color.swift
+++ backlog/Model/Color.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import SwiftData
+import SwiftUI
+
+@Model
+final class ColorModel: Identifiable {
+    var id = UUID()
+    var red: CGFloat
+    var green: CGFloat
+    var blue: CGFloat
+    var alpha: CGFloat
+
+    init(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha
+    }
+
+    convenience init(color: UIColor) {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+
+    var uiColor: UIColor {
+        UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
+    var swiftUIColor: Color {
+        Color(uiColor)
+    }
+
+    static func from(color: Color) -> ColorModel {
+        let uiColor = UIColor(color)
+        return ColorModel(color: uiColor)
+    }
+}
