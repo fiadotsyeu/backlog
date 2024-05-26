1d65514f8c3b059456a89bd4798cf5cccc96f39c
DreF <fiadotsyeu@gmail.com>
2024-05-26 12:09:13 +0200

    Added itemRow file

---
+++ backlog/View/ItemRow.swift
@@ -0,0 +1,18 @@
+//
+//  ItemRow.swift
+//  backlog
+//
+//  Created by Andrei Fiadotsyeu on 25.05.2024.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemRow()
+}
