b2914583d16dc9ef64131901c7919262f841fea2
DreF <fiadotsyeu@gmail.com>
2024-06-01 21:10:10 +0200

    Added file

---
+++ backlog/View/ArchivView.swift
@@ -0,0 +1,18 @@
+//
+//  ArchivView.swift
+//  backlog
+//
+//  Created by Andrei Fiadotsyeu on 01.06.2024.
+//
+
+import SwiftUI
+
+struct ArchivView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArchivView()
+}
