0a3c3f3eb0bd5e7a5c9aa1a3ace5e754b62d43a5
DreF <fiadotsyeu@gmail.com>
2024-06-01 17:52:41 +0200

    The project data has been updated.

--- backlog/backlogApp.swift
+++ backlog/backlogApp.swift
@@ -13,6 +13,7 @@ struct backlogApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
+            Tag.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
