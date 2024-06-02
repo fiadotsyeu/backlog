cb24eae56e1c78566a15a6af82dbd99c4cbf3d41
DreF <fiadotsyeu@gmail.com>
2024-06-02 13:08:25 +0200

    Archived items are now displayed here. A search function has also been added.

--- backlog/View/ArchivView.swift
+++ backlog/View/ArchivView.swift
@@ -6,13 +6,45 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ArchivView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+    @State private var searchText = ""
+    
+    private var searchResults : [Item] {
+        searchText.isEmpty ? items : items.filter { $0.title.localizedStandardContains(searchText) }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationSplitView {
+            VStack {
+                CustomSearchBar(searchText: $searchText)
+                    .padding(.vertical, 15)
+                List {
+                    Section(header: Text("Archived items")) {
+                        ForEach(searchResults.filter { $0.isArchive }, id: \.self) { item in
+                            NavigationLink(destination: DetailView(item: item)) {
+                                ItemRow(item: item)
+                            }
+                        }
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
+                .animation(.default, value: items.count)
+            }
+            .listStyle(.plain)
+            .animation(.default, value: searchResults)
+        } detail: {
+            
+        }
+        .scrollIndicators(.hidden)
+        .navigationViewStyle(.stack)
     }
 }
 
 #Preview {
     ArchivView()
+        .modelContainer(for: Item.self, inMemory: true)
 }
