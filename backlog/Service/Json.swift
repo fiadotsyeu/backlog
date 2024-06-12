9914771e2d70064a7d779176e675908c14912f1a
DreF <fiadotsyeu@gmail.com>
2024-06-12 17:21:04 +0200

    Added file

---
+++ backlog/Service/Json.swift
@@ -0,0 +1,18 @@
+//
+//  Json.swift
+//  backlog
+//
+//  Created by Andrei Fiadotsyeu on 12.06.2024.
+//
+
+import SwiftUI
+
+struct Json: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Json()
+}
