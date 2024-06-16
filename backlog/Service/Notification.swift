d9c598c4aed1a9a5e1ae914ab3ab5f3b8e8a0e59
DreF <fiadotsyeu@gmail.com>
2024-06-16 10:13:00 +0200

    Added file

---
+++ backlog/Service/Notification.swift
@@ -0,0 +1,18 @@
+//
+//  Notification.swift
+//  backlog
+//
+//  Created by Andrei Fiadotsyeu on 16.06.2024.
+//
+
+import SwiftUI
+
+struct Notification: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Notification()
+}
