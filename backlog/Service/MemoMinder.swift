a5e1a95bc94982ef71e926a2c8d34ec68f69c923
DreF <fiadotsyeu@gmail.com>
2024-06-16 09:38:06 +0200

    Added file

---
+++ backlog/Service/MemoMinder.swift
@@ -0,0 +1,18 @@
+//
+//  MemoMinder.swift
+//  backlog
+//
+//  Created by Andrei Fiadotsyeu on 16.06.2024.
+//
+
+import SwiftUI
+
+struct MemoMinder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MemoMinder()
+}
