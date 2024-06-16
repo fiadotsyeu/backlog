514d7abb79e817ceef195d9bbeb8c8fc89afac53
DreF <fiadotsyeu@gmail.com>
2024-06-16 16:16:37 +0200

    MemoMinder functionality implemented. Timers are created within the specified range, but not earlier than 7 AM and not later than 9 PM.

--- backlog/Service/MemoMinder.swift
+++ backlog/Service/MemoMinder.swift
@@ -6,10 +6,51 @@
 //
 
 import SwiftUI
+import SwiftData
+import Foundation
 
-struct MemoMinder: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class MemoMinder {
+    var creationDate: Date
+    var modificationDate: Date
+    var period: TimeInterval
+    var notificationId: String
+    var useModificationDate: Bool
+    
+    init(creationDate: Date, modificationDate: Date, period: TimeInterval, useModificationDate: Bool = false) {
+        self.creationDate = creationDate
+        self.modificationDate = modificationDate
+        self.period = period
+        self.notificationId = UUID().uuidString
+        self.useModificationDate = useModificationDate
+        NotificationManager.shared.registerFileTimer(self)
+    }
+    
+    func scheduleNotification() {
+        let notificationDate = getRandomDateWithinPeriod()
+        print("notificationDate: \(customDateFormatter.string(from: notificationDate)) notificationId: \(notificationId)")
+        NotificationManager.shared.scheduleNotification(at: notificationDate, with: notificationId)
+    }
+    
+    private func getRandomDateWithinPeriod() -> Date {
+        let startDate = useModificationDate ? modificationDate : creationDate
+        var randomDate: Date
+        
+        repeat {
+            let randomTimeInterval = TimeInterval.random(in: 0..<period)
+            randomDate = startDate.addingTimeInterval(randomTimeInterval)
+        } while randomDate <= Date() || !isWithinAllowedHours(randomDate)
+        
+        return randomDate
+    }
+    
+    private func isWithinAllowedHours(_ date: Date) -> Bool {
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        return hour >= 7 && hour < 21
+    }
+    
+    func updateForReschedule() {
+        useModificationDate = true
     }
 }
 
