ca16f19004ec75940a0f4b5e7b09bfcb7d93d614
DreF <fiadotsyeu@gmail.com>
2024-06-22 16:25:09 +0200

    Added file

---
+++ backlog/View/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  backlog
+//
+//  Created by Andrei Fiadotsyeu on 22.06.2024.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
