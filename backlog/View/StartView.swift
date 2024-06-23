65974424996b1e36452202aa521fe53c44939a72
DreF <fiadotsyeu@gmail.com>
2024-06-23 15:58:24 +0200

    View added at application startup

--- backlog/View/StartView.swift
+++ backlog/View/StartView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct StartView: View {
+    
+    @State private var scale = 0.7
+    @Binding var isActive: Bool
+    @AppStorage("activateDarkMode") private var activateDarkMode: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(activateDarkMode ? "app_logo_dark" : "app_logo_light")
+                .resizable()
+                .frame(width: 160, height: 40)
+            Text("Effortless Content Management for Your Busy Life")
+                .foregroundColor(Color(activateDarkMode ? .white : .black))
+                .font(.system(.footnote))
+                .bold()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(activateDarkMode ? .black : .white))
+        .ignoresSafeArea(.all)
+        .onAppear{
+            withAnimation(.easeIn(duration: 0.7)) {
+                self.scale = 0.9
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
     }
-}
-
-#Preview {
-    StartView()
 }
