f88400f1837207a1e3fb2f6381d482d82e773f9d
DreF <fiadotsyeu@gmail.com>
2024-06-26 18:11:35 +0200

    Json class added

--- backlog/Service/Json.swift
+++ backlog/Service/Json.swift
@@ -6,10 +6,70 @@
 //
 
 import SwiftUI
+import SwiftData
+import UniformTypeIdentifiers
 
-struct Json: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+class Json {
+    struct JsonDocument: FileDocument {
+        static var readableContentTypes: [UTType] {
+            [.json]
+        }
+                
+        var text = ""
+                
+        init(text: String = "") {
+            self.text = text
+        }
+                
+        init(configuration: ReadConfiguration) throws {
+            if let data = configuration.file.regularFileContents {
+                text = String(decoding: data, as: UTF8.self)
+            } else {
+                text = ""
+            }
+        }
+                
+        func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+            FileWrapper(regularFileWithContents: Data(text.utf8))
+        }
+        
+        
+        static func encodeToJson(_ items: [Item], _ tags: [Tag]) -> String {
+            let combinedData = CombinedData(items: items, tags: tags)
+            let encoder = JSONEncoder()
+            encoder.outputFormatting = .prettyPrinted
+            do {
+                let jsonData = try encoder.encode(combinedData)
+                if let jsonString = String(data: jsonData, encoding: .utf8) {
+                    return jsonString
+                } else {
+                    print("Failed to convert JSON data to UTF-8 string")
+                    return ""
+                }
+            } catch {
+                print("Error encoding JSON: \(error)")
+                return ""
+            }
+        }
+        
+        
+        static func importFromJson(jsonString: String) -> (items: [Item], tags: [Tag])? {
+            guard let jsonData = jsonString.data(using: .utf8) else {
+                print("Failed to convert JSON string to data")
+                return nil
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .secondsSince1970
+                let combinedData = try decoder.decode(CombinedData.self, from: jsonData)
+                return (combinedData.items, combinedData.tags)
+            } catch {
+                print("Error decoding JSON: \(error)")
+                return nil
+            }
+        }
     }
 }
 
